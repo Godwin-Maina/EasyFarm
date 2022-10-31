fe0ebb745c6b9c71d510854b3b3628c2b8f7c4e0
Godwin-Maina <goddymaina6@gmail.com>
2022-10-31 08:33:20 +0300

    HELLO MASTER.

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your  EasyFarm app!</Text>
+      <Text>HELLO WORLD MASTER.</Text>
       <StatusBar style="auto" />
     </View>
   );
